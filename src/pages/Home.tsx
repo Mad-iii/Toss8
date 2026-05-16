44f39e5ee59ca0b1b681e36f66ab4b7a037846ff
Mad-iii <156696189+Mad-iii@users.noreply.github.com>
2026-05-16 12:09:31 +0000

    Update bANNER

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -77,7 +77,7 @@ const Home = () => {
                     <img 
                         src="/images/Banner.png" 
                         alt="Hero background" 
-                        className="w-full h-full object-cover opacity-50"
+                        className="w-full h-full object-cover opacity-20"
                     />
                 </div>
             </div>
